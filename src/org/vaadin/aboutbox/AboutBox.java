a66d9a7e59367593b25c105dd1abb4d4005931bd
eq <sami.ekblad@gmail.com>
2011-07-15 18:49:48 +0300

    1.0.0 published

--- src/org/vaadin/aboutbox/AboutBox.java
+++ src/org/vaadin/aboutbox/AboutBox.java
@@ -16,11 +16,22 @@ import com.vaadin.ui.Alignment;
 public class AboutBox extends AbstractComponent {
 
     private static final long serialVersionUID = 3838190603396285099L;
-    private String title;
+    private static final String VERSION = "1.0.0";
+
+    public static final String DEFAULT_STYLE = VAboutBox.STYLE_DEFAULT;
+    private static final String DEFAULT_TITLE = "About<br />" + VERSION;
+    private static final String DEFAULT_CAPTION = "About";
+    private static final String DEFAULT_DESCRIPTION = "";
+    private static final String DEFAULT_LINK_CAPTION = null;
+    private static final Alignment DEFAULT_ALIGNMENT = Alignment.TOP_RIGHT;
+    private static final int DEFAULT_OFFSET = 100;
     private String linkCaption;
+    private String title;
     private ClickListener listener;
     private Alignment alignment;
     private int offset;
+    private boolean open;
+    private boolean sendOpen;
 
     /** Listener callback interface */
     public interface ClickListener {
@@ -28,6 +39,32 @@ public class AboutBox extends AbstractComponent {
         /** Invoked when user clicks the about box link. */
         public void aboutBoxClicked(AboutBox sender);
 
+    }
+
+    /**
+     * Create new AboutBox.
+     *
+     */
+    public AboutBox() {
+        setTitle(DEFAULT_TITLE);
+        setCaption(DEFAULT_CAPTION);
+        setDescription(DEFAULT_DESCRIPTION);
+        setLinkCaption(DEFAULT_LINK_CAPTION);
+        setAlignment(DEFAULT_ALIGNMENT);
+        setOffset(DEFAULT_OFFSET);
+        setStyleName(DEFAULT_STYLE);
+    }
+
+    /**
+     * Create new AboutBox.
+     *
+     */
+    public AboutBox(String title, String description) {
+        this();
+        setTitle(title);
+        setCaption(DEFAULT_CAPTION);
+        setDescription(DEFAULT_DESCRIPTION);
+        setLinkCaption(DEFAULT_LINK_CAPTION);
     }
 
     /**
@@ -41,22 +78,27 @@ public class AboutBox extends AbstractComponent {
      */
     public AboutBox(String caption, String title, String description,
             String linkCaption, ClickListener linkListener) {
-        super();
+        this(title, description);
         setCaption(caption);
-        setTitle(title);
-        setDescription(description);
         setLinkCaption(linkCaption);
         setListener(linkListener);
+        setStyleName(DEFAULT_STYLE);
     }
 
     @Override
     public void paintContent(PaintTarget target) throws PaintException {
         super.paintContent(target);
-        target.addAttribute("style", getStyleName());
+        target.addAttribute("boxStyle", getStyleName());
         target.addAttribute("title", getTitle());
-        target.addAttribute("linkCaption", getLinkCaption());
-        target.addAttribute("align", alignment.getBitMask());
+        if (getLinkCaption() != null && !"".equals(linkCaption)) {
+            target.addAttribute("linkCaption", getLinkCaption());
+        }
+        target.addAttribute("align", getAlignment().getBitMask());
         target.addAttribute("offset", offset);
+        if (sendOpen) {
+            target.addAttribute("open", open);
+        }
+        sendOpen = false;
     }
 
     /**
@@ -86,6 +128,12 @@ public class AboutBox extends AbstractComponent {
         return title;
     }
 
+    @Override
+    public void setDescription(String description) {
+        super.setDescription(description != null ? description
+                : DEFAULT_DESCRIPTION);
+    }
+
     public void setLinkCaption(String linkCaption) {
         this.linkCaption = linkCaption;
         requestRepaint();
@@ -105,7 +153,37 @@ public class AboutBox extends AbstractComponent {
 
     @Override
     public void setStyleName(String style) {
-        super.setStyleName(style);
+        super.setStyleName(style != null ? style : DEFAULT_STYLE);
+    }
+
+    public void open() {
+        open = true;
+        sendOpen = true;
+        requestRepaint();
+    }
+
+    public void close() {
+        open = false;
+        sendOpen = true;
+        requestRepaint();
+    }
+
+    public void setOffset(int newOffset) {
+        offset = newOffset <= 0 ? 0 : newOffset;
+        requestRepaint();
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setAlignment(Alignment alignment) {
+        this.alignment = alignment != null ? alignment : DEFAULT_ALIGNMENT;
+        requestRepaint();
+    }
+
+    public Alignment getAlignment() {
+        return alignment;
     }
 
 }
--- src/org/vaadin/aboutbox/client/ui/VAboutBox.java
+++ src/org/vaadin/aboutbox/client/ui/VAboutBox.java
@@ -1,22 +1,32 @@
 package org.vaadin.aboutbox.client.ui;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Style.Cursor;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.client.Paintable;
 import com.vaadin.terminal.gwt.client.UIDL;
+import com.vaadin.terminal.gwt.client.ui.AlignmentInfo;
 
 /**
  * Client side widget which communicates with the server. Messages from the
  * server are shown as HTML and mouse clicks are sent to the server.
  */
-public class VAboutBox extends Widget implements Paintable, ClickHandler {
+public class VAboutBox extends Widget implements Paintable {
 
     /** Set the CSS class name to allow styling. */
     public static final String CLASSNAME = "v-aboutbox";
+
+    private static final String CLASS_ABOUTBOX = "about";
 
     public static final String CLICK_EVENT_IDENTIFIER = "lc";
 
@@ -27,6 +37,14 @@ public class VAboutBox extends Widget implements Paintable, ClickHandler {
     private static final String CLASS_LINK = "l";
 
     private static final String CLASS_CAPTION = "c";
+
+    private static final String CLASS_SHADOW = "s";
+
+    public static final String STYLE_DEFAULT = "leather";
+
+    public static final String STYLE_NONE = "none";
+
+    private static final String CSS_TEME_ID = "aboutbox-theme";
 
     /** The client side widget identifier */
     protected String paintableId;
@@ -44,14 +62,31 @@ public class VAboutBox extends Widget implements Paintable, ClickHandler {
 
     private DivElement linkDiv;
 
+    private String boxStyle = STYLE_NONE;
+
+    private DivElement shadowDiv;
+
+    private AlignmentInfo alignment;
+
+    private int offset;
+
+    private DivElement rootDiv;
+
+    private boolean disableLink;
+
     /**
      * The constructor should first call super() to initialize the component and
      * then handle any initialization relevant to Vaadin.
      */
     public VAboutBox() {
-        aboutDiv = Document.get().createDivElement();
-        setElement(aboutDiv);
+        rootDiv = Document.get().createDivElement();
+        setElement(rootDiv);
         setStyleName(CLASSNAME);
+
+        // The main title when expanded
+        aboutDiv = Document.get().createDivElement();
+        aboutDiv.setClassName(CLASS_ABOUTBOX);
+        rootDiv.appendChild(aboutDiv);
 
         // The main title when expanded
         titleDiv = Document.get().createDivElement();
@@ -73,56 +108,168 @@ public class VAboutBox extends Widget implements Paintable, ClickHandler {
         captionDiv.setClassName(CLASS_CAPTION);
         aboutDiv.appendChild(captionDiv);
 
-        // TODO: Click handler
+        // Shadow
+        shadowDiv = Document.get().createDivElement();
+        shadowDiv.setClassName(CLASS_SHADOW);
+        rootDiv.appendChild(shadowDiv);
+
+        // Click handler
+        addDomHandler(new ClickHandler() {
+
+            public void onClick(final ClickEvent event) {
+                if (!disableLink &&  event.getNativeEvent().getEventTarget() == (Object) linkDiv) {
+                    linkClicked();
+                } else {
+                    toggleOpen();
+                }
+            }
+        }, ClickEvent.getType());
+
+        addDomHandler(new MouseOverHandler() {
+
+            public void onMouseOver(MouseOverEvent event) {
+                highlight();
+            }
+
+        }, MouseOverEvent.getType());
+
+        addDomHandler(new MouseOutHandler() {
+
+            public void onMouseOut(MouseOutEvent event) {
+                unHighlight();
+            }
+        }, MouseOutEvent.getType());
+
     }
 
     /**
      * Called whenever an update is received from the server
      */
     public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
-        if (client.updateComponent(this, uidl, true)) {
+        if (client.updateComponent(this, uidl, false)) {
             return;
         }
 
         this.client = client;
         paintableId = uidl.getId();
 
+        String style = uidl.getStringAttribute("boxStyle");
+        if (!boxStyle.equals(style)) {
+            setBoxStyle(style);
+        }
         String caption = uidl.getStringAttribute("caption");
-        String title = uidl.getStringAttribute("title");
-        String description = uidl.getStringAttribute("description");
-        String linkCaption = uidl.getStringAttribute("linkCaption");
-        boolean open = uidl.getBooleanAttribute("open");
-
         captionDiv.setInnerText(caption);
+
+        String title = uidl.getStringAttribute("title");
         titleDiv.setInnerHTML(title);
+
+        String description = uidl.getStringAttribute("description");
         descriptionDiv.setInnerHTML(description);
-        linkDiv.setInnerText(linkCaption);
+
+        if (uidl.hasAttribute("linkCaption")) {
+            String linkCaption = uidl.getStringAttribute("linkCaption");
+            linkDiv.setInnerText(linkCaption);
+            disableLink = false;
+            linkDiv.getStyle().setCursor(Cursor.POINTER);
+        } else {
+            disableLink = true;
+            linkDiv.getStyle().setCursor(Cursor.AUTO);
+        }
+
+        alignment = new AlignmentInfo(uidl.getIntAttribute("align"));
+        offset = uidl.getIntAttribute("offset");
+
+        // Apply box position
+        applyPosition();
 
         // Open or close
-        if (open) {
-            open();
-        } else {
-            close();
+        if (uidl.hasAttribute("open")) {
+            if (uidl.getBooleanAttribute("open")) {
+                open();
+            } else {
+                close();
+            }
         }
 
     }
 
+    private void applyPosition() {
+
+        // Defaults
+        if (alignment == null) {
+            alignment = new AlignmentInfo(AlignmentInfo.RIGHT,
+                    AlignmentInfo.TOP);
+        }
+        if (offset < 0) {
+            offset = 100;
+        }
+
+        if (alignment.isRight()) {
+            aboutDiv.getStyle().setRight(offset - 10, Unit.PX);
+            aboutDiv.getStyle().clearLeft();
+            shadowDiv.getStyle().setRight(offset, Unit.PX);
+            shadowDiv.getStyle().clearLeft();
+        } else {
+            aboutDiv.getStyle().setLeft(offset-10, Unit.PX);
+            aboutDiv.getStyle().clearRight();
+            shadowDiv.getStyle().setLeft(offset, Unit.PX);
+            shadowDiv.getStyle().clearRight();
+        }
+
+    }
+
+    private void setBoxStyle(String boxStyle) {
+        this.boxStyle = boxStyle;
+        loadCss(GWT.getModuleBaseURL() + "aboutbox/" + boxStyle + ".css",
+                "all", CSS_TEME_ID);
+    }
+
+    private native void loadCss(String url, String media, String id) /*-{
+
+                                                                     var links = $doc.getElementsByTagName("link");
+                                                                     var es = null;
+                                                                     if (links.length > 0) {
+                                                                     for (var i = 0; i<links.length; i++) {
+                                                                     if (links[i]["id"] == id) {
+                                                                     if (links[i]["href"] == url) {
+                                                                      return;
+                                                                     } else {
+                                                                        es = links[i];
+                                                                        break;
+                                                                     }
+                                                                     }
+                                                                     }
+                                                                     }
+
+                                                                     if (es == null) {
+                                                                     es = $doc.createElement("link");
+                                                                     }
+                                                                     es.setAttribute("id", id);
+                                                                     es.setAttribute("rel", "stylesheet");
+                                                                     es.setAttribute("type", "text/css");
+                                                                     es.setAttribute("media", media);
+                                                                     es.setAttribute("href", url);
+
+                                                                     var head = $doc.getElementsByTagName("head")[0];
+                                                                     head.appendChild(es);
+                                                                     }-*/;
+
     /**
      * Called when a native click event is fired.
      *
-     * @param event
-     *            the {@link ClickEvent} that was fired
      */
-    public void onClick(ClickEvent event) {
+    public void linkClicked() {
         client.updateVariable(paintableId, CLICK_EVENT_IDENTIFIER, true, true);
     }
 
     /**
-     * Highlights the AboutBox
+     * Highlights the AboutBox when it is not open.
      *
      */
     public void highlight() {
-        aboutDiv.addClassName("hl");
+        if (!isOpen()) {
+            aboutDiv.addClassName("hl");
+        }
     }
 
     /**
@@ -138,7 +285,28 @@ public class VAboutBox extends Widget implements Paintable, ClickHandler {
      *
      */
     public void open() {
+        unHighlight();
         aboutDiv.addClassName("open");
+    }
+
+    /**
+     * Open or close the about box depending on its current state.
+     */
+    protected void toggleOpen() {
+        if (isOpen()) {
+            close();
+        } else {
+            open();
+        }
+    }
+
+    /**
+     * Is the about box currently open.
+     *
+     * @return
+     */
+    public boolean isOpen() {
+        return aboutDiv.getClassName().indexOf("open") >= 0;
     }
 
     /**
